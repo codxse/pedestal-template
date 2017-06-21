c48320c240d9244b7357bbb5216a295dfe7af96a
Nadiar AS <codxse@gmail.com>
2017-06-21 23:38:45 +0700

    croot ignore

--- resources/public/js/compiled/desktop/out/figwheel/connect/build_desktop_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-desktop-dev (:require [app.system] [figwheel.client] [figwheel.client.utils] [cljs.user]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/app.system.reset (apply js/app.system.reset x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'app.system/reset' is missing"))), :build-id "desktop-dev", :websocket-url "ws://localhost:3450/figwheel-ws"})
-
